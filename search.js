687889bd3bedab1eb29e220f4450093e3204d002
JuSung Lee <jusunglee1459@gmail.com>
2018-09-15 17:47:56 -0700

    algolia setup

---
+++ search.js
@@ -0,0 +1,11 @@
+// var algoliasearch = require('algoliasearch');
+// var algoliasearch = require('algoliasearch/reactnative');
+// var algoliasearch = require('algoliasearch/lite');
+// import * as algoliasearch from 'algoliasearch'; // When using TypeScript
+
+// or just use algoliasearch if you are using a <script> tag
+// if you are using AMD module loader, algoliasearch will not be defined in window,
+// but in the AMD modules of the page
+
+var client = algoliasearch('3G0F33T1FV', '0added83576c84f234874a9e336f06e7');
+var index = client.initIndex('index');
